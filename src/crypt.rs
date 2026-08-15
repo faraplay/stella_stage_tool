65d3732ce6b3e04bb5a4ade3fa30404584ccf8f6
faraplay <faraplay@outlook.com>
2026-08-15 13:30:51 +0100

    load whole file into memory

--- src/crypt.rs
+++ src/crypt.rs
@@ -4,16 +4,17 @@ use std::{
     path::Path,
 };
 
+use aes::{
+    Aes192,
+    cipher::{Array, BlockModeDecrypt, KeyIvInit},
+};
+use cbc::Decryptor;
 use flate2::bufread::ZlibDecoder;
 
-use self::{aes_read::AesDecryptor, error::CryptError};
-
-mod aes_read;
-mod error;
 mod prng;
 
 /// Decrypt all files in a directory. Searches the directory recursively.
-pub fn decrypt_directory(in_path: &Path, out_path: &Path) -> Result<(), CryptError> {
+pub fn decrypt_directory(in_path: &Path, out_path: &Path) -> std::io::Result<()> {
     // try to create output directory
     let create_result = create_dir(out_path);
     match create_result {
@@ -46,53 +47,54 @@ pub fn decrypt_directory(in_path: &Path, out_path: &Path) -> Result<(), CryptErr
 }
 
 /// Decrypts a file.
-pub fn decrypt_file(in_path: &Path, out_path: &Path) -> Result<(), CryptError> {
+pub fn decrypt_file(in_path: &Path, out_path: &Path) -> std::io::Result<()> {
     let mut reader = File::open(in_path)?;
     let mut writer = File::create(out_path)?;
     decrypt_stream(&mut reader, &mut writer)?;
     Ok(())
 }
 
-fn decrypt_stream(
-    reader: &mut (impl Read + Seek),
-    writer: &mut impl Write,
-) -> Result<(), CryptError> {
+fn decrypt_stream(reader: &mut (impl Read + Seek), writer: &mut impl Write) -> std::io::Result<()> {
     let size = reader.seek(SeekFrom::End(0))?;
     if size % 16 != 0 {
-        return Err(CryptError::file_size_error(
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
             "File size is not a multiple of 16!",
         ));
     }
     if size < 0x30 {
-        return Err(CryptError::file_size_error("File to decrypt is too small!"));
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "File to decrypt is too small!",
+        ));
     }
     reader.seek(SeekFrom::Start(0))?;
-    let mut kb_buffer = vec![0u8; std::cmp::min(size, 0x400) as usize];
-    reader.read_exact(&mut kb_buffer)?;
-    decrypt_first_kb(&mut kb_buffer, size)?;
-    let iv_seed: u64 = u32::from_le_bytes(kb_buffer[0x24..0x28].try_into().unwrap()).into();
+    let mut in_data = Vec::new();
+    reader.read_to_end(&mut in_data)?;
+
+    decrypt_first_kb(&mut in_data[..std::cmp::min(size, 0x400) as usize], size)?;
+
+    let iv_seed: u64 = u32::from_le_bytes(in_data[0x24..0x28].try_into().unwrap()).into();
     let decompressed_file_size: u64 =
-        u32::from_le_bytes(kb_buffer[0x28..0x2c].try_into().unwrap()).into();
+        u32::from_le_bytes(in_data[0x28..0x2c].try_into().unwrap()).into();
 
     let aes_key = get_aes_key(size);
     let aes_iv = get_iv(iv_seed.into());
-    let mut decompressed_reader = ZlibDecoder::new(AesDecryptor::new(
-        (&kb_buffer[0x30..]).chain(reader),
-        &aes_key,
-        &aes_iv,
-    ));
+    decrypt(&aes_key, &aes_iv, &mut in_data[0x30..])?;
+    let mut decompressed_reader = ZlibDecoder::new(&in_data[0x30..]);
     let out_file_size = std::io::copy(&mut decompressed_reader, writer)?;
     assert_eq!(decompressed_file_size, out_file_size);
     Ok(())
 }
 
-fn decrypt_first_kb(kb_buffer: &mut [u8], seed: u64) -> Result<(), CryptError> {
+fn decrypt_first_kb(kb_buffer: &mut [u8], seed: u64) -> std::io::Result<()> {
     let mut my_prng = prng::MyPrng::new(seed);
     for _ in 0..5 {
         my_prng.next_u64();
     }
     let (chunks, []) = kb_buffer.as_chunks_mut::<4>() else {
-        return Err(CryptError::file_size_error(
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
             "File size is not a multiple of 4!",
         ));
     };
@@ -131,4 +133,16 @@ fn get_iv(seed: u64) -> [u8; 16] {
         aes_iv[4 * i..4 * (i + 1)].copy_from_slice(&value.to_le_bytes());
     }
     aes_iv
+}
+
+fn decrypt(key: &[u8; 24], iv: &[u8; 16], data: &mut [u8]) -> std::io::Result<()> {
+    let mut decryptor = Decryptor::<Aes192>::new(key.into(), iv.into());
+    let (chunks, []) = Array::slice_as_chunks_mut(data) else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Buffer fill count is not a multiple of 16!",
+        ));
+    };
+    decryptor.decrypt_blocks(chunks);
+    Ok(())
 }
